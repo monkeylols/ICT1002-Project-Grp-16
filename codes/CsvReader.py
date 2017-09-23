96a1445a79fc898fd3daa704c6db68a2513f4b38
keybabyx <31960316+keybabyx@users.noreply.github.com>
2017-09-23 12:14:22 +0800

    Update CsvReader.py

--- codes/CsvReader.py
+++ codes/CsvReader.py
@@ -1,6 +1,7 @@
 import csv
 from Tkinter import Tk
 from tkFileDialog import askopenfilename
+from dateutil import parser
 
 import FeedbackInfo
 import os
@@ -63,3 +64,39 @@ for i in range(len(feedbackinfo_list)):
         f.write(feedbackinfo_list[i].write_content())
         f.close
         a +=1'''
+
+# YT - function 3
+num_feedback = {}
+num_complaints = {}
+for i in range(len(feedbackinfo_list)):
+    report_date_time = getattr(feedbackinfo_list[i], 'report_date_time')
+    category = getattr(feedbackinfo_list[i], 'category')
+    
+    try:
+        date = parser.parse(report_date_time).strftime('%d/%m/%Y')
+    except ValueError:
+        try:
+            date = datetime.datetime.strptime(report_date_time, '%d %b %Y %H%M hrs').strftime('%d/%m/%Y')
+        except ValueError:
+            print "Unknown Date Format"
+
+    if category == 'Feedback':
+        if date in num_feedback:
+            num_feedback[date] += 1
+        else:
+            num_feedback[date] = 1
+    elif category == 'Complaints':
+        if date in num_complaints:
+            num_complaints[date] += 1
+        else:
+            num_complaints[date] = 1
+
+# YT - function 4
+request_type = {}
+for i in range(len(feedbackinfo_list)):
+    request = getattr(feedbackinfo_list[i], 'des_type')
+    if request: 
+        if request in request_type:
+            request_type[request] += 1
+        else:
+            request_type[request] = 1
