85c923d5f3ff6afeafa30eed3e224ebc49c97f85
keybabyx <31960316+keybabyx@users.noreply.github.com>
2017-10-07 10:03:52 +0800

    Update NumberOfType.py

--- codes/NumberOfType.py
+++ codes/NumberOfType.py
@@ -1,12 +1,25 @@
 import CsvReader
 
+# Function 4
+def get_request_type(feedbackinfo_list):
+    request_type = {}
+    types = {'Air Conditioning': ['Air', 'Acmv'], 'Building': ['Building', 'Floor'], 'Carpark & Driveway': ['Carpark', 'Driveway', 'Pothole'],
+             'Cleanliness': ['Cleaning', 'Cleanliness'], 'Electrical': ['Electrical'], 'Fire': ['Fire'], 'Lift': ['Lift'], 'Lighting': ['Lighting'],
+             'Others': ['Other'], 'Pest Control': ['Pest'], 'Maintenance': ['Drainage', 'Plumbing', 'Pump', 'Toilet', 'Water', 'Tree', 'Waste', 'Mechanical'],
+             'Security & Services': ['Security', 'Safety', 'Service', 'Equipment']}
+
+    for i in feedbackinfo_list:
+        request = getattr(i, 'des_type').title()
+        if request:
+            for key, value in types.items():
+                for val in value:
+                    if val in request.title():
+                        if key in request_type:
+                            request_type[key] += 1
+                        else:
+                            request_type[key] = 1
+    for k, v in request_type.items():
+        print "%s: %d" %(k, v)
 
 feedbackinfo_list = CsvReader.read_file()
-request_type = {}
-for i in range(len(feedbackinfo_list)):
-    request = getattr(feedbackinfo_list[i], 'des_type')
-    if request:
-        if request in request_type:
-            request_type[request] += 1
-        else:
-            request_type[request] = 1
+get_request_type(feedbackinfo_list)
