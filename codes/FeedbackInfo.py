24f961b18f722daddd0a168429ec93a14bda110b
cjunhui <31959963+cjunhui@users.noreply.github.com>
2017-10-18 11:42:04 +0800

    update2

---
+++ codes/FeedbackInfo.py
@@ -0,0 +1,47 @@
+class FeedbackInfo:
+    def __init__(self, report_date_time, company, requestor, property_name, category, des_type, location, if_breakdown,
+               description, finding, action_taken, start_date_time, acknowledge_date_time, completed_date_time, status,
+               customer_type):
+        self.report_date_time = report_date_time
+        self.company = company
+        self.requestor = requestor
+        self.property_name = property_name
+        self.category = category
+        self.des_type = des_type
+        self.location = location
+        self.if_breakdown = if_breakdown
+        self.description = description
+        self.finding = finding
+        self.action_taken = action_taken
+        self.start_date_time = start_date_time
+        self.acknowledge_date_time = acknowledge_date_time
+        self.completed_date_time = completed_date_time
+        self.status = status
+        self.customer_type = customer_type
+
+    def display_content(self):
+        print self.report_date_time + ", " + self.company + ", " + self.requestor + ", " + self.property_name + ", " \
+              + self.category + ", " + self.des_type + ", " + self.location + ", " + self.if_breakdown + ", " \
+              + self.description + ", " + self.finding + ", " + self.action_taken + ", " + self.start_date_time \
+              + ", " + self.acknowledge_date_time + ", " + self.completed_date_time + ", " + self.status + ", " \
+              + self.customer_type
+
+    def write_content(self):
+        S = "ReportDateTime:\t%s\n"%self.report_date_time
+        S+= "Company:\t%s\n"%self.company
+        S+= "Requestor:\t%s\n"%self.requestor
+        S+= "PropertyName:\t%s\n"%self.property_name
+        S+= "Category:\t%s\n"%self.category
+        S+= "Des_type:\t%s\n"%self.des_type
+        S+= "Location:\t%s\n"%self.location
+        S+= "Breakdown:\t%s\n"%self.if_breakdown
+        S+= "Description:\t%s\n"%self.description
+        S+= "Finding:\t%s\n"%self.finding
+        S+= "ActionTaken:\t%s\n"%self.action_taken
+        S+= "StartDateTime\t%s\n"%self.start_date_time
+        S+= "AcknowledgeDateTime:\t%s\n"%self.acknowledge_date_time
+        S+= "CompletedDateTime:\t%s\n"%self.completed_date_time
+        S+= "Status:\t%s\n"%self.status
+        S+= "CustomerType:\t%s"%self.customer_type
+        return S
+
---
+++ codes/NewEntry.py
@@ -0,0 +1,24 @@
+import CsvReader
+import csv
+import datetime
+
+def new_entry(feedbackinfo_list):
+    new_fbi_list = CsvReader.read_file()
+    feedbackinfo_list += new_fbi_list
+
+    with open('../data/' + str(datetime.datetime.now().mircrosecond) + '.csv', 'wb') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(
+            ['Reported on', 'Co. Name', 'Ext. Requestor', 'Property Name', 'Category', 'Order Group Description',
+             'Floor/unit or space', 'Breakdown?\n(Yes/No)', 'Description', 'Nature of feedback/complants (& Finding)',
+             'Action taken', 'Start date & time', 'Acknowledged date', 'Technically completed on', 'Status',
+             'Customer/ FED Internal'])
+
+        for item in feedbackinfo_list:
+            wr.writerow(
+                [item.report_date_time, item.company, item.requestor, item.property_name, item.category, item.des_type,
+                 item.location, item.if_breakdown, item.description, item.finding, item.action_taken,
+                 item.start_date_time, item.acknowledge_date_time, item.completed_date_time, item.status,
+                 item.customer_type])
+
+    return feedbackinfo_list
